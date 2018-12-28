6879153f3fb03fa59041af607525b64f35599a60
rrsilaya <rrsilaya@gmail.com>
2018-12-28 18:39:48 +0800

    Setup Travis CI

---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+set -x
+
+cp package.json build && \
+tar -czf package.tgz build && \
+scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
+ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
---
+++ scripts/untar.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+set -x
+
+cd $REMOTE_APP_DIR && \
+tar zxvf package.tgz -C . && \
+mv build/package.json . && \
+yarn install && \
+node build/main.js
