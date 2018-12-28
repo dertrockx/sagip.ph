64b2473eaa59645eccb2ca978469b0789a7b0eeb
rrsilaya <rrsilaya@gmail.com>
2018-12-28 21:28:54 +0800

    Update PATH ENV

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -x
 
-source /home/rrsilaya/.bashrc && \
+PATH=/home/rrsilaya/.nvm/versions/node/v10.8.0/bin/:$PATH && \
 cd /home/rrsilaya/apps/sagip.ph && \
 tar zxvf package.tgz -C . && \
 mv build/package.json . && \
