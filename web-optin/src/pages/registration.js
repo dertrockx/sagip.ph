8568c60d472964fd2f2b7ab275d2f4c918fcea25
rrsilaya <rrsilaya@gmail.com>
2019-04-19 15:33:17 +0800

    Modify form submission

--- web-optin/src/pages/registration.js
+++ web-optin/src/pages/registration.js
@@ -14,18 +14,28 @@ const professions = [
 ];
 
 class Registration extends Component {
+  state = { code: '' };
+
+  componentDidMount() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
+
+    this.setState({ code });
+  }
+
   render() {
     return (
       <div className="container container-padded">
         <h1 className="heading-1 heading-gutter-bottom">Continue Registration</h1>
         <p className="typography-sub">To continue with your registration, please fill out all the necessary details below.</p>
 
-        <form className="registration" method="POST">
+        <form className="registration" method="POST" action="/v1/register">
           <div className="fields">
-            <Input type="text" placeholder="Juan Dela Cruz" label="Name" required />
-            <Input type="date" label="Birthdate" required />
-            <Input type="select" label="Profession" placeholder="Select Profession" selectOptions={professions} required />
-            <Input type="text" label="Affiliation" placeholder="N/A"/>
+            <input type="text" className="input-hidden" value={this.state.code} name="code" disabled />
+            <Input type="text" placeholder="Juan Dela Cruz" label="Name" name="name" required />
+            <Input type="date" label="Birthdate" name="birthdate" required />
+            <Input type="select" label="Profession" name="profession" placeholder="Select Profession" selectOptions={professions} required />
+            <Input type="text" label="Affiliation" name="affiliation" placeholder="N/A"/>
           </div>
 
           <button className="submit" type="submit">Register</button>
