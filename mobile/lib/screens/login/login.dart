79f3551677cde6fd6fb1d33d2912cd2e5dd8c4e1
rrsilaya <rrsilaya@gmail.com>
2019-04-19 18:56:10 +0800

    Integrate login

--- mobile/lib/screens/login/login.dart
+++ mobile/lib/screens/login/login.dart
@@ -1,9 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:io';
+import 'dart:convert';
 import 'package:sagip/config/theme.dart';
 
 import 'package:sagip/components/buttons/primary.dart';
 
-class Login extends StatelessWidget {
+class _Login extends State<Login> {
+  final mobileNumberController = TextEditingController();
+
+  Future<Map<String, dynamic>> login() async {
+    this._openLoader();
+
+    final data = { 'phoneNumber': this.mobileNumberController.text };
+    final res = await http.post(
+      'https://api.sagip.ph/v1/login',
+      headers: { HttpHeaders.contentTypeHeader: 'application/json' },
+      body: json.encode(data)
+    );
+
+    Navigator.of(context).pop();
+
+    debugPrint(res.body);
+    return json.decode(res.body);
+  }
+
+  Future<void> _openLoader() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Logging in'),
+          content: SingleChildScrollView(
+            child: Center(child: CircularProgressIndicator())
+          )
+        );
+      }
+    );
+  }
+
+  @override
+  void dispose() {
+    this.mobileNumberController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget _input = TextField(
@@ -12,6 +54,7 @@ class Login extends StatelessWidget {
       keyboardType: TextInputType.number,
       textAlign: TextAlign.center,
       style: largeText.merge(invertedText),
+      controller: this.mobileNumberController
     );
 
     return Material(
@@ -30,12 +73,13 @@ class Login extends StatelessWidget {
               PrimaryButton(
                 color: whiteColor,
                 textColor: primaryColor,
-                onPressed: () => (
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    '/dashboard',
-                    (Route<dynamic> route) => false
-                  )
-                ),
+                onPressed: this.login,
+                // onPressed: () => (
+                //   Navigator.of(context).pushNamedAndRemoveUntil(
+                //     '/dashboard',
+                //     (Route<dynamic> route) => false
+                //   )
+                // ),
                 child: Text('Login', style: mediumText),
               ),
             ]
@@ -44,4 +88,9 @@ class Login extends StatelessWidget {
       )
     );
   }
+}
+
+class Login extends StatefulWidget {
+  @override
+  _Login createState() => _Login();
 }
