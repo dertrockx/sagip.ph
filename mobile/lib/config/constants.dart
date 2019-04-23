d5aca020571e01354d516b7eb833aba0ebb22ee2
rrsilaya <rrsilaya@gmail.com>
2019-04-23 10:23:42 +0800

    Update list of disasters

--- mobile/lib/config/constants.dart
+++ mobile/lib/config/constants.dart
@@ -2,8 +2,14 @@ final String APP_ID = 'j6M6UpExGEu67ca9KbTxxzu8o6XAU7Ed';
 
 final DISASTER_LIST = <String> [
   'Extreme Flooding',
+  'Landslide',
+  'Tsunami',
+  'Volcanic Erruption',
+  'Tropical Cyclone',
   'Earthquake',
-  'Fire'
+  'Fire',
+  'Tornado',
+  'Others',
 ];
 
 final String APP_SHORT_CODE = '21589027';
